7ebc1eab79c7ceeeb4b0ab71102255ef1343325b
AriSwift <6937523@qq.com>
2019-05-03 15:55:50 +0800

    Initial Commit

---
+++ CrashyPlane/GameViewController.swift
@@ -0,0 +1,57 @@
+//	
+//        _                  _     ______                  _       ___    _    
+//       / \                (_)  .' ____ \                (_)    .' ..]  / |_  
+//      / _ \      _ .--.   __   | (___ \_|  _   _   __   __    _| |_   `| |-' 
+//     / ___ \    [ `/'`\] [  |   _.____`.  [ \ [ \ [  ] [  |  '-| |-'   | |   
+//   _/ /   \ \_   | |      | |  | \____) |  \ \/\ \/ /   | |    | |     | |,  
+//  |____| |____| [___]    [___]  \______.'   \__/\__/   [___]  [___]    \__/ 
+//  GitHub: https://github.com/AnnieAri
+//  GameViewController.swift 
+//  CrashyPlane 
+// 
+//  Created by AriSwift on 2019/5/3. 
+//  Copyright © 2019 AriSwift. All rights reserved. 	
+// 
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
